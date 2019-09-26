1a9b50f9fe37c1a5e450a377807ed45a27225695
wangting <739515806@qq.com>
2019-09-26 17:38:32 +0800

    测试git不同文件的提交

--- src/main/java/com/zyzx/springboot/controller/HelloContrloller.java
+++ src/main/java/com/zyzx/springboot/controller/HelloContrloller.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloContrloller {
     @RequestMapping("/hello")
     public String hello(){
+        System.out.println("测试git不同文件的提交");
 
         return "hello man";
     }
